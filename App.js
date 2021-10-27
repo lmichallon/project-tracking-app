9216601fc49e91276ed7274a1d79f7f970adf6ed
unknown <Administrateur@PRT-MN-443.marenostrum.local>
2021-10-27 15:56:55 +0200

    addStrapi

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import {useState} from "react";
 
 export default function App() {
+    const [database, setDatabase] = useState([])
+    axios.get('http://localhost:1337/projects').then((res) => {
+        setDatabase(res.data)
+    })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your apps!</Text>
       <StatusBar style="auto" />
+        <Core data={database} />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
